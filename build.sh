c5f41119f3c121eaf19a71e7c4f77ef199203a88
MossabAhmed <mosab2003medhat@gmail.com>
2026-02-10 19:03:18 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,13 @@ python manage.py collectstatic --no-input
 
 # تطبيق الترحيلات لقاعدة البيانات
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'YourPassword123')
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
+END
